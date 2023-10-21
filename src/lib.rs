303d96ae5c542c1a0e5c2cea4261cbdd3ffee228
Peter Kehl <peter.kehl@gmail.com>
2023-10-21 03:13:59 -0700

    part, sub_part

--- src/lib.rs
+++ src/lib.rs
@@ -13,19 +13,16 @@ use core::mem;
 /// Using [`alloc::collections::vec_deque::VecDeque`] would make the implementation easier, but less
 /// efficient (because its lower bound is not necessarily `0`, but movable around the ring buffer.
 /// Also, [`Vec`] is more common.
-pub type Storage<T, const N: usize> = [Vec<T>; N];
+pub type Storage<T> = [Vec<T>; 2];
+pub type InputAndStorage<T> = (Vec<T>, Storage<T>);
 
-fn debug_assert_all_empty<T, const N: usize>(storage: &Storage<T, N>) {
-    debug_assert!({
-        for i in 0..N {
-            debug_assert!(storage[i].is_empty());
-        }
-        true
-    });
+fn debug_assert_all_empty<T>(storage: &Storage<T>) {
+    debug_assert!(storage[0].is_empty());
+    debug_assert!(storage[1].is_empty());
 }
 
 /// Assert that all [`Vec`] in `storage` have sufficient capacity (equal to, or greater than, `capacity`).
-fn debug_assert_all_capacity<T>(storage: &Storage<T, 2>, capacity: usize) {
+fn debug_assert_all_capacity<T>(storage: &Storage<T>, capacity: usize) {
     debug_assert!(storage[0].capacity() >= capacity);
     debug_assert!(storage[1].capacity() >= capacity);
 }
@@ -44,10 +41,10 @@ fn debug_assert_all_capacity<T>(storage: &Storage<T, 2>, capacity: usize) {
 // to avoid shuffling.
 #[must_use]
 pub fn qsort<T, CONSUME>(
-    mut input: Vec<T>,
-    consume: CONSUME,
-    storage: Storage<T, 2>,
-) -> Storage<T, 3>
+    input: Vec<T>,
+    consume: &CONSUME,
+    storage: Storage<T>,
+) -> InputAndStorage<T>
 where
     T: Ord,
     CONSUME: Fn(usize, T) -> bool,
@@ -55,7 +52,8 @@ where
     debug_assert_all_empty(&storage);
     debug_assert_all_capacity(&storage, input.len());
 
-    loop {}
+    let mut next_out_seq_idx = 0usize;
+    part(input, consume, &mut next_out_seq_idx, storage)
 }
 
 /// - `next_out_seq_idx`: 0-based index of the next output item (increasing by one per each output
@@ -65,8 +63,8 @@ fn part<T, CONSUME>(
     mut input: Vec<T>,
     consume: &CONSUME,
     next_out_seq_idx: &mut usize,
-    storage: Storage<T, 2>,
-) -> (Vec<T>, Storage<T, 2>)
+    storage: Storage<T>,
+) -> InputAndStorage<T>
 where
     T: Ord,
     CONSUME: Fn(usize, T) -> bool,
@@ -87,13 +85,14 @@ where
             greater_equal.push(value);
         }
     }
-    // We reuse `input`, consuming it, then returning (moving) it back.
-    let input = match lower.len() {
-        0 => input,
+    // We reuse `input` and `lower`, consuming them, then returning (moving) them back.
+    let (input, lower) = sub_part(input, lower, consume, next_out_seq_idx);
+    /*let (input, lower) = match lower.len() {
+        0 => (input, lower),
         1 => {
             consume(*next_out_seq_idx, lower.pop().unwrap());
             *next_out_seq_idx += 1;
-            input
+            (input, lower)
         }
         2 => {
             // Let's save splitting & reconstructing the Storage vectors: sort 2 items manually.
@@ -106,18 +105,63 @@ where
             *next_out_seq_idx += 1;
             consume(*next_out_seq_idx, two);
             *next_out_seq_idx += 1;
-            input
+            (input, lower)
         }
         lower_len => {
-            let (storage, original_capacity) =
-                unsafe { split_vec(input, lower_len, lower_len) };
+            let (storage, original_capacity) = unsafe { split_vec(input, lower_len, lower_len) };
             let (lower, storage) = part(lower, consume, next_out_seq_idx, storage);
-            unsafe { join_vecs(storage, original_capacity) }
+            (unsafe { join_vecs(storage, original_capacity) }, lower)
         }
-    };
+    };*/
     consume(*next_out_seq_idx, pivot);
     *next_out_seq_idx += 1;
-    loop {}
+    let (input, greater_equal) = sub_part(input, greater_equal, consume, next_out_seq_idx);
+    (input, [lower, greater_equal])
+}
+
+#[must_use]
+fn sub_part<T, CONSUME>(
+    mut input: Vec<T>,
+    mut sub: Vec<T>,
+    consume: &CONSUME,
+    next_out_seq_idx: &mut usize,
+) -> (Vec<T>, Vec<T>)
+where
+    T: Ord,
+    CONSUME: Fn(usize, T) -> bool,
+{
+    debug_assert!(input.is_empty());
+    // We reuse `input`: splitting it (by consuming it), then receiving it back in parts,
+    // re-constructing it and returning (moving) it back.
+    //
+    // We also consume, receive & return `sub`.
+    match sub.len() {
+        0 => (input, sub),
+        1 => {
+            consume(*next_out_seq_idx, sub.pop().unwrap());
+            *next_out_seq_idx += 1;
+            (input, sub)
+        }
+        2 => {
+            // Let's save splitting & reconstructing the Storage vectors: sort 2 items manually.
+            let mut one = sub.pop().unwrap();
+            let mut two = sub.pop().unwrap();
+            if one > two {
+                mem::swap(&mut one, &mut two);
+            }
+            consume(*next_out_seq_idx, one);
+            *next_out_seq_idx += 1;
+            consume(*next_out_seq_idx, two);
+            *next_out_seq_idx += 1;
+            (input, sub)
+        }
+        sub_len => {
+            let (storage, original_capacity) = unsafe { split_vec(input, sub_len, sub_len) };
+            let (sub, storage) = part(sub, consume, next_out_seq_idx, storage);
+            debug_assert_eq!(sub.len(), sub_len);
+            (unsafe { join_vecs(storage, original_capacity) }, sub)
+        }
+    }
 }
 
 /// Similar to [`[T]::split_at_mut()`]:
@@ -139,7 +183,7 @@ unsafe fn split_vec<T>(
     input: Vec<T>,
     capacity_one: usize,
     capacity_two: usize,
-) -> (Storage<T, 2>, usize) {
+) -> (Storage<T>, usize) {
     debug_assert!(capacity_one + capacity_two <= input.capacity());
     loop {}
 }
@@ -148,7 +192,7 @@ unsafe fn split_vec<T>(
 /// [`Drop::drop`] it (them) automatically, or before you pass it outside this module (for re-use).
 ///
 /// Only pass two adjacent [`Vec`]-tors returned from the same call to [`split_vec`].
-unsafe fn join_vecs<T>(vecs: Storage<T, 2>, original_capacity: usize) -> Vec<T> {
+unsafe fn join_vecs<T>(vecs: Storage<T>, original_capacity: usize) -> Vec<T> {
     loop {}
 }
 
