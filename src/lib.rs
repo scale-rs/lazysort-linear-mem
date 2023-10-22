8196b809565434d89c72e79b75e5db7dda7167c8
Peter Kehl <peter.kehl@gmail.com>
2023-10-22 00:16:24 -0700

    VecDequeSplit & CrossVecTempTakePair - initial.

--- src/lib.rs
+++ src/lib.rs
@@ -2,28 +2,162 @@
 #![feature(vec_into_raw_parts)]
 extern crate alloc;
 
-use alloc::vec::Vec;
-use core::{mem, ops::Deref};
+use alloc::{
+    collections::{vec_deque, VecDeque},
+    vec::Vec,
+};
+use core::{
+    marker::PhantomData,
+    mem,
+    ops::{Deref, Drop},
+};
 
-struct Fro {}
-impl core::ops::Drop for Fro {
-    fn drop(&mut self) {
-        panic!()
+/// A contract on top of [`VecDeque`]. It (logically) keeps two heaps, growing in the opposite
+/// directions toward each other. Similar to how stack & heap grow toward each other (in a single
+/// threaded process/OS).
+///
+/// At any time, [VecDequeSplit::vec_deque]`.len()` equals to[`VecDequeSplit::front`] +
+/// [`VecDequeSplit::back`].
+struct VecDequeSplit<'a, T> {
+    vec_deque: &'a mut VecDeque<T>,
+    /// Front (left) side length.
+    front: usize,
+    /// Back (right) side length.
+    back: usize,
+}
+
+impl<'a, T> VecDequeSplit<'a, T> {
+    fn new_from_empty(vec_deque: &'a mut VecDeque<T>) -> Self {
+        debug_assert!(vec_deque.is_empty());
+        // Once .pop_front() or .pop_back() empty the VecDeque completely, according to their source
+        // code (see linked from
+        // <https://doc.rust-lang.org/nightly/alloc/collections/vec_deque/struct.VecDeque.html#method.pop_front>
+        // and
+        // <https://doc.rust-lang.org/nightly/alloc/collections/vec_deque/struct.VecDeque.html#method.pop_back>)
+        // they do NOT ensure/reset the indices to 0 (to be contiguous). So we ensure it.
+        vec_deque.make_contiguous();
+        Self {
+            vec_deque,
+            front: 0,
+            back: 0,
+        }
+    }
+
+    fn push_front(&mut self, value: T) {
+        self.vec_deque.push_front(value);
+        self.front += 1;
+        self.debug_assert_consistent();
+    }
+    fn push_back(&mut self, value: T) {
+        self.vec_deque.push_back(value);
+        self.back += 1;
+        self.debug_assert_consistent();
+    }
+
+    #[inline(always)]
+    fn debug_assert_consistent(&self) {
+        debug_assert_eq!(self.front + self.back, self.vec_deque.len());
     }
 }
-struct In {}
-impl From<Fro> for In {
-    fn from(_: Fro) -> Self {
-        Self {}
+
+struct CrossVecPair<T>(Vec<T>, Vec<T>);
+
+/// A wrapper around two [`Vec`]s based on (backed by, shadowing) the same [`VecDequeSplit`].
+///
+/// After use, the original [`VecDequeSplit::vec_deque`] may be corrupted.
+///
+/// At the end of use, call [`CrossVecPair::forget()`]. Do not let it go out of scope in any other
+/// way
+/// - otherwise its [`Drop::drop()`] will panic.
+struct CrossVecTempTakePair<'c, 'vds, T> {
+    /// Always [Some]. ([None] is used only during [Drop::drop].)
+    ///
+    /// The two [`Vec`]s correspond to [`VecDequeSplit::front`] & [`VecDequeSplit::back`],
+    /// respectively.
+    ///
+    /// 'unsafe' and potentially invalid (while it's "temporarily taken"). Do NOT access directly.
+    /// Instead, use [CrossVecTempTakePair]'s functions ONLY.
+    pair: Option<CrossVecPair<T>>,
+    /// Whether the (whole) pair was temporarily "taken" (as if moved out).
+    temp_taken: bool,
+    phantom_vec_deque_split: PhantomData<&'c mut VecDequeSplit<'vds, T>>,
+}
+impl<'c, 'vds, T> CrossVecTempTakePair<'c, 'vds, T> {
+    /// We do NOT implement [`From`], because its `from` function is not declared unsafe.
+    #[must_use]
+    unsafe fn new_from(vec_deque_split: &'c mut VecDequeSplit<'vds, T>) -> Self {
+        let (front, back) = vec_deque_split.vec_deque.as_mut_slices();
+        let front = Vec::from_raw_parts(front.as_mut_ptr(), front.len(), front.len());
+        let back = Vec::from_raw_parts(back.as_mut_ptr(), back.len(), back.len());
+        Self {
+            pair: Some(CrossVecPair(front, back)),
+            temp_taken: false,
+            phantom_vec_deque_split: PhantomData,
+        }
+    }
+
+    /// "Take" the (whole). Like "moving out".
+    ///
+    /// We need this temporary "move out" ability, so that we can then transform the [`Vec`]
+    /// into[`VecDeque`] in the next deeper recursion level. We do it with
+    /// <https://doc.rust-lang.org/nightly/alloc/collections/vec_deque/struct.VecDeque.html#impl-From%3CVec%3CT,+A%3E%3E-for-VecDeque%3CT,+A%3E>,
+    /// which takes the [`Vec`] by value (move).
+    ///
+    /// Once you're finished using the pair, undo this with
+    /// [CrossVecTempTakePair::move_temp_taken_back()]. You must do so before calling
+    /// [CrossVecTempTakePair::forget()].
+    #[must_use]
+    fn temp_take(&mut self) -> CrossVecPair<T> {
+        debug_assert!(!self.temp_taken, "Already 'temporarily taken.'");
+        self.temp_taken = true;
+
+        fn cross_vec<T>(v: &mut Vec<T>) -> Vec<T> {
+            let len = v.len();
+            let capacity = v.capacity();
+            unsafe { Vec::from_raw_parts(v.as_mut_ptr(), len, capacity) }
+        }
+        let current = self.pair.as_mut().unwrap();
+        CrossVecPair(cross_vec(&mut current.0), cross_vec(&mut current.1))
+    }
+
+    /// Check that the parameter `pair` are [`Vec`]s based on this [CrossVecTempTakePair] instance.
+    /// Then "move" the pair back.
+    fn move_temp_taken_back(&mut self, pair: CrossVecPair<T>) {
+        debug_assert!(self.temp_taken, "Not 'temporarily taken.'");
+        self.temp_taken = false;
+
+        let current = self.pair.as_ref().unwrap();
+        // We do NOT compare length, since it may have drifted to be different.
+        debug_assert_eq!(pair.0.as_ptr(), current.0.as_ptr());
+        debug_assert_eq!(pair.1.as_ptr(), current.1.as_ptr());
+        self._forget_pair();
+        self.pair = Some(pair);
+    }
+
+    fn _forget_pair(&mut self) {
+        debug_assert!(!self.temp_taken, "'Temporarily taken.'");
+        let pair = self.pair.take();
+        let CrossVecPair(front, back) = pair.unwrap();
+        front.into_raw_parts();
+        back.into_raw_parts();
+    }
+
+    /// Call this before the instance goes out of scope. If the pair was "temporarily taken" with
+    /// [CrossVecTempTakePair::temp_take()], use [CrossVecTempTakePair::move_temp_taken_back()] first.
+    fn forget(mut self) {
+        self._forget_pair();
+    }
+}
+impl<'c, 'vds, T> Drop for CrossVecTempTakePair<'c, 'vds, T> {
+    fn drop(&mut self) {
+        debug_assert!(!self.temp_taken, "'Temporarily taken.'");
+        debug_assert!(self.pair.is_none());
     }
 }
 #[cfg(test)]
 mod test {
     #[test]
-    fn convert_not_invoking_drop() {
-        let fro = super::Fro {};
-        let into: super::In = fro.into();
-    }
+    fn convert_not_invoking_drop() {}
 }
 
 /// Array of two mutable [`Vec`] references.
@@ -77,7 +211,8 @@ fn debug_assert_empty<T>(store_pair: &StorePair<T>) {
     debug_assert!(store_pair[1].is_empty());
 }
 
-/// Assert that all [`Vec`] in `storage` have sufficient capacity (equal to, or greater than, `capacity`).
+/// Assert that all [`Vec`] in `storage` have sufficient capacity (equal to, or greater than,
+/// `capacity`).
 #[inline(always)]
 fn debug_assert_capacity<T>(store_pair: &StorePair<T>, capacity: usize) {
     debug_assert!(store_pair[0].capacity() >= capacity);
@@ -247,7 +382,8 @@ where
         input_len => {
             let store_orig_capacity = store_single.capacity();
 
-            // Set the capacity to any possible maximum, but not more - so that we catch any errors a.s.a.p.
+            // Set the capacity to any possible maximum, but not more - so that we catch any errors
+            // a.s.a.p.
             //
             // Oh!:  2*side_len may be MORE than store_single.capacity!
             let store_pair = unsafe { split_vec(store_single.0, input_len, input_len) };
